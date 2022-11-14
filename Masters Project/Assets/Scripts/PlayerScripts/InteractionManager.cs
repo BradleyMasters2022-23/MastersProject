82ee25cd9511e55a21c6f7e1e220b8c22f2df336
soma h <41604440+whybeekeeper@users.noreply.github.com>
2022-11-14 13:00:17 -0600

    BACKUP

--- Masters Project/Assets/Scripts/PlayerScripts/InteractionManager.cs
+++ Masters Project/Assets/Scripts/PlayerScripts/InteractionManager.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InteractionManager : MonoBehaviour
 {
+    private PlayerController player;
+
     private void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+
+    }
+
+    private void Update()
     {
         
     }
