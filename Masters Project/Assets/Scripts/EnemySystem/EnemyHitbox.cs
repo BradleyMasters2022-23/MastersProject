f6d46497be6c76767b183f64e448aafdf134fd40
TySenseiSama <taichefyah@gmail.com>
2022-12-10 03:54:26 -0600

    Sound hooks
    All sound hooks in

--- Masters Project/Assets/Scripts/EnemySystem/EnemyHitbox.cs
+++ Masters Project/Assets/Scripts/EnemySystem/EnemyHitbox.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Cinemachine.DocumentationSortingAttribute;
+using static Unity.VisualScripting.Member;
+using UnityEngine.Rendering;
 
 public class EnemyHitbox : Damagable
 {
@@ -8,6 +11,14 @@ public class EnemyHitbox : Damagable
     [SerializeField] private float damageMultiplier = 1;
 
     private EnemyHealth enemy;
+
+    [Tooltip("Weak spot sound effect")]
+    [SerializeField] private AudioClip weakPoint;
+    [Tooltip("Armored spot sound effect")]
+    [SerializeField] private AudioClip armoredPoint;
+    [Tooltip("Sound when its a basic hit")]
+    [SerializeField] private AudioClip basicHit;
+
 
     private void Start()
     {
@@ -20,10 +31,25 @@ public class EnemyHitbox : Damagable
         int modifiedDamage = Mathf.CeilToInt(_dmg * damageMultiplier);
 
         // Testing logs
-        //if (damageMultiplier > 1)
-        //    Debug.Log("enemy taking extra damage from vulnerable point!");
-        //else if (damageMultiplier < 1)
-        //    Debug.Log("enemy taking less damage from armored point!");
+        if (damageMultiplier > 1)
+        {
+            //Debug.Log("enemy taking extra damage from vulnerable point!");
+            if(weakPoint!=null)
+                AudioSource.PlayClipAtPoint(weakPoint, Camera.main.transform.position,0.5f);
+        }
+        else if (damageMultiplier < 1)
+        {
+            //Debug.Log("enemy taking less damage from armored point!");
+            if (armoredPoint != null)
+                AudioSource.PlayClipAtPoint(armoredPoint, Camera.main.transform.position, 0.5f);
+        }
+        else
+        {
+            //Debug.Log("Enemy takng hit from basic attack!");
+            if (basicHit != null)
+                AudioSource.PlayClipAtPoint(basicHit, Camera.main.transform.position, 0.5f);
+        }
+            
 
         enemy.Damage(modifiedDamage);
     }
@@ -31,5 +57,9 @@ public class EnemyHitbox : Damagable
     protected override void Die()
     {
         return;
+    }
+    private void Awake()
+    {
+        source = gameObject.AddComponent<AudioSource>();
     }
 }
