9fc75b7e3fe208bb532adf5a90b7753eba085342
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2022-12-12 17:37:40 -0600

    sprint and no jump bug fixed

--- Masters Project/Assets/Scripts/PlayerScripts/AimController.cs
+++ Masters Project/Assets/Scripts/PlayerScripts/AimController.cs
@@ -117,10 +117,14 @@ public class AimController : MonoBehaviour
 
         // Manage horizontal rotation
         transform.rotation *= Quaternion.AngleAxis(lookDelta.x * sensitivity * horizontalInversion * Time.deltaTime, Vector3.up);
+        
+
 
         // Manage vertical rotaiton
         Quaternion temp = cameraLook.transform.localRotation *
             Quaternion.AngleAxis(-lookDelta.y * sensitivity * verticalInversion * Time.deltaTime, Vector3.right);
+
+        Debug.Log("Mouse vertical change sensitivity: " + temp.eulerAngles);
 
         // Make sure to clamp vertical angle first
         float newYAngle = temp.eulerAngles.x;
