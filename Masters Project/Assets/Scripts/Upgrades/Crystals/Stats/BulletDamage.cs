8f3617cc99bedb3e16d4a785ea546d5b6e022982
soma h <41604440+whybeekeeper@users.noreply.github.com>
2023-04-07 10:56:14 -0500

    backup

--- Masters Project/Assets/Scripts/Upgrades/Crystals/Stats/BulletDamage.cs
+++ Masters Project/Assets/Scripts/Upgrades/Crystals/Stats/BulletDamage.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class BulletDamage : IStat
 {
+    private PlayerGunController gun;
+    private int totalMod;
+
+    /// <summary>
+    /// Multiplies with the mod to get the amount to increase the total damage modifier by.
+    /// </summary>
+    [SerializeField] private float statBase;
     public override void LoadStat(PlayerController player, int mod)
     {
-
+        gun = player.gameObject.GetComponent<PlayerGunController>();
+        gun.SetDamageMultiplier((float)mod * statBase + gun.GetDamageMultiplier());
     }
 
     public override void UnloadStat(PlayerController player, int mod)
     {
-
+        gun = player.gameObject.GetComponent<PlayerGunController>();
+        gun.SetDamageMultiplier(gun.GetDamageMultiplier() - ((float)mod * statBase));
     }
 }
--- Masters Project/Assets/Scripts/Upgrades/Crystals/Stats/BulletRange.cs
+++ Masters Project/Assets/Scripts/Upgrades/Crystals/Stats/BulletRange.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class BulletRange : IStat
 {
+    private PlayerGunController gun;
     public override void LoadStat(PlayerController player, int mod)
     {
-
+        
     }
 
     public override void UnloadStat(PlayerController player, int mod)
