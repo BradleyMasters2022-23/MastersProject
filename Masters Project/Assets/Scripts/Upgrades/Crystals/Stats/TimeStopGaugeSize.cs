a858a6708dbde5158db246ada58982265a7659e5
soma h <41604440+whybeekeeper@users.noreply.github.com>
2023-04-17 13:56:44 -0500

    time

--- Masters Project/Assets/Scripts/Upgrades/Crystals/Stats/TimeStopGaugeSize.cs
+++ Masters Project/Assets/Scripts/Upgrades/Crystals/Stats/TimeStopGaugeSize.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class TimeStopGaugeSize : IStat
 {
+    private TimeManager time;
+    [SerializeField] private float statBase;
     public override void LoadStat(PlayerController player, int mod)
     {
-
+        time = FindObjectOfType<TimeManager>();
+        time.SetGaugeMax(((float)mod/10) + statBase);
     }
 
     public override void UnloadStat(PlayerController player, int mod)
     {
-
+        time = FindObjectOfType<TimeManager>();
+        time.SetGaugeMax(1 / (((float)mod / 10) + statBase));
     }
 
     public override float GetStatIncrease(int mod)
     {
-        return 1;
+        return ((float)mod*10);
     }
 }
