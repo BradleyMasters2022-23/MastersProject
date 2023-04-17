a52e83355fdb09ca01faccd652257e5efa661e50
soma h <41604440+whybeekeeper@users.noreply.github.com>
2023-04-17 11:07:59 -0500

    stats-wip

--- Masters Project/Assets/Scripts/Upgrades/Crystals/Stats/JumpHeight.cs
+++ Masters Project/Assets/Scripts/Upgrades/Crystals/Stats/JumpHeight.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class JumpHeight : IStat
 {
+    [SerializeField] private float statBase;
     public override void LoadStat(PlayerController player, int mod)
     {
-
+        float newForce;
+        newForce = player.GetJumpForce() + (((float)mod * statBase) * player.GetJumpForce());
+        player.SetJumpForce(newForce);
     }
 
     public override void UnloadStat(PlayerController player, int mod)
     {
-
+        float newForce;
+        newForce = player.GetJumpForce() - (((float)mod * statBase) * player.GetJumpForce());
+        player.SetJumpForce(newForce);
     }
 
     public override float GetStatIncrease(int mod)
     {
-        return 1;
+        PlayerController player = FindObjectOfType<PlayerController>();
+        return ((float)mod * statBase) * player.GetJumpForce();
     }
 }
