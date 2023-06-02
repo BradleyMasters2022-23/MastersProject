dbcfae69380b1dc01117468b221a982c7247fe5c
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2023-06-02 11:56:45 -0500

    more optimization
    Removed legacy stuff from map segments to reduce load they have

--- Masters Project/Assets/Scripts/Maps/MapGeneration/MapSegmentSO.cs
+++ Masters Project/Assets/Scripts/Maps/MapGeneration/MapSegmentSO.cs
@@ -6,28 +6,28 @@ using Sirenix.OdinInspector;
 [CreateAssetMenu(fileName = "New Map Segment", menuName = "Gameplay/Map Segment")]
 public class MapSegmentSO : ScriptableObject
 {
-    public enum MapSegmentType
-    {
-        Room,
-        Hallway,
-        FinalRoom
-    }
+    //public enum MapSegmentType
+    //{
+    //    Room,
+    //    Hallway,
+    //    FinalRoom
+    //}
 
-    [Tooltip("Type of map section this is")]
-    [EnumToggleButtons, PropertySpace(8, 8)]
-    public MapSegmentType segmentType;
+    //[Tooltip("Type of map section this is")]
+    //[EnumToggleButtons, PropertySpace(8, 8)]
+    //public MapSegmentType segmentType;
 
-    [Tooltip("The prefab containing this map section")]
-    [PropertySpace(0, 8), AssetsOnly, PreviewField( Alignment = ObjectFieldAlignment.Center, Height = 200)]
-    public GameObject segmentPrefab;
+    //[Tooltip("The prefab containing this map section")]
+    //[PropertySpace(0, 8), AssetsOnly, PreviewField( Alignment = ObjectFieldAlignment.Center, Height = 200)]
+    //public GameObject segmentPrefab;
 
     [Tooltip("Name of the scene this level has")]
     public string sceneName;
 
-    [ShowIf("@this.segmentType == MapSegmentType.Room")]
+    //[ShowIf("@this.segmentType == MapSegmentType.Room")]
     [Tooltip("The minimum difficulty level this room can be used in")]
     public int minDifficulty = 0;
-    [ShowIf("@this.segmentType == MapSegmentType.Room")]
+    //[ShowIf("@this.segmentType == MapSegmentType.Room")]
     [Tooltip("The maximum difficulty level this room can be used in")]
     public int maxDifficulty = 999;
 
