e90760b7f15be42bd41fc583d22168274406eeec
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2023-06-26 14:23:38 -0500

    UI communication polish

--- Masters Project/Assets/Scripts/InputSystem/InputManager.cs
+++ Masters Project/Assets/Scripts/InputSystem/InputManager.cs
@@ -319,14 +319,14 @@ public class InputManager : MonoBehaviour
                     // update prompt so player knows whats going on
                     FindObjectOfType<TwoChoiceMenu>(true).SetMainPrompt(
                         $"{targetAction.GetBindingDisplayString(targetIdx)} is already in use by {action.action.name}." +
-                        $"\nIf you continue, these bindings will be switched.");
+                        $"\n\nSwap keybinds for {targetAction.name} and {action.action.name}?");
 
                     // Open the two choice window always in rebind menu. Pass in the two options for the player
                     FindObjectOfType<TwoChoiceMenu>(true).Open(
                         ()=> SwapControls(Controls.FindAction(action.action.id.ToString()), i, onComplete), 
                         "Swap Keybindings", 
                         ()=> UndoRebind(onComplete), 
-                        "Undo Keybind");
+                        "Cancel");
 
                     break;
                 }
@@ -368,6 +368,7 @@ public class InputManager : MonoBehaviour
     }
 
     #endregion
+
 
     #region ActionLookup
 
