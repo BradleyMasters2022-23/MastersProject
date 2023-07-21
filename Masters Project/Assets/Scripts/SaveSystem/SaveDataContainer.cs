e774dd503dc5453b336d6344c5ff8f3d50b7f8a4
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2023-07-21 13:08:48 -0500

    Update SaveDataContainer.cs

--- Masters Project/Assets/Scripts/SaveSystem/SaveDataContainer.cs
+++ Masters Project/Assets/Scripts/SaveSystem/SaveDataContainer.cs
@@ -1,9 +1,17 @@
+/* ================================================================================================
+ * Author - Ben Schuster   
+ * Date Created - July 21st, 2023
+ * Last Edited - July 21st, 2023 by Ben Schuster
+ * Description - Abstract class for any container that stores save data
+ * ================================================================================================
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class SaveDataContainer : MonoBehaviour
 {
+    [Tooltip("Channel called when data is cleared. Will reset this container's loaded data")]
     [SerializeField] ChannelVoid ResetSaveChannel;
 
     protected virtual void OnEnable()
@@ -15,5 +23,8 @@ public abstract class SaveDataContainer : MonoBehaviour
         ResetSaveChannel.OnEventRaised -= ResetData;
     }
 
+    /// <summary>
+    /// Reset the current data, but don't save it
+    /// </summary>
     public abstract void ResetData();
 }
